7be800fff103041cae9de252379d4d6bd6004a76
ushekate <ushekate@gmail.com>
2025-07-18 16:37:50 +0530

    First Commit...

--- src/app/(software)/client/(after_login)/cfs/services/pricing/components/page.jsx
+++ src/app/(software)/client/(after_login)/cfs/services/pricing/components/page.jsx
@@ -14,7 +14,9 @@ import {
     File,
     FileImage,
     Scan,
-    Wrench
+    Wrench,
+    ArrowLeftRight,
+    Send
 } from 'lucide-react';
 
 export default function ViewPricingRequest() {
@@ -58,7 +60,7 @@ export default function ViewPricingRequest() {
         <div className="p-4 space-y-6 min-h-screen">
             <div className="py-2 border-b-4 border-secondary/50">
                 <h1 className="flex gap-2 font-semibold text-2xl text-black">
-                    <FileText /> View Pricing Request - #{requestId}
+                    <FileText size={24} className='mt-1' /> View Pricing Request - #{requestId}
                 </h1>
             </div>
 
@@ -66,12 +68,12 @@ export default function ViewPricingRequest() {
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 {/* Customer Profile */}
                 <div className="bg-white shadow rounded-xl p-5 text-black space-y-2">
-                    <h3 className="text-lg font-semibold text-black flex items-center gap-2">
+                    <h3 className="text-lg font-semibold text-black flex items-center gap-2 pb-2">
                         <User className="w-5 h-5 text-foreground fill-foreground" /> Customer Profile
                     </h3>
                     <div className='flex gap-4 items-center'>
-                        <Building size={50} className='text-secondary bg-secondary/20 p-2 rounded-full' />
-                        <p className="font-semibold text-lg text-black border-b-4 border-secondary/50">{customer.company}</p>
+                        <Building size={50} className='text-foreground bg-foreground/20 p-2 rounded-full' />
+                        <p className="font-semibold text-lg text-black border-b-4 border-foreground/60">{customer.company}</p>
                     </div>
                     <p className="text-sm flex items-center gap-1"><CircleUser size={18} /><span className="font-medium">Contact Person:</span> {customer.person}</p>
                     <p className="text-sm flex items-center gap-1 text-foreground"><Mail size={18} className="w-4 h-4 text-black" /><span className='font-medium text-black'>Email:</span> {customer.email}</p>
@@ -80,7 +82,7 @@ export default function ViewPricingRequest() {
 
                 {/* Request Details */}
                 <div className="bg-white col-span-2 shadow text-black rounded-xl p-5 space-y-2">
-                    <h3 className="text-lg font-semibold flex items-center gap-2">
+                    <h3 className="text-lg font-semibold flex items-center gap-2 pb-2">
                         <ClipboardList className="w-5 h-5 text-blue-600" /> Request Details
                     </h3>
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
@@ -109,7 +111,7 @@ export default function ViewPricingRequest() {
                     {documents.map((doc, index) => (
                         <div key={index} className="border border-secondary/50 rounded-lg px-4 py-2 bg-white flex items-center gap-2">
                             <span className="flex gap-1 font-medium">
-                                <doc.icon size={18} className={`${doc.color} mt-1`}  />
+                                <doc.icon size={18} className={`${doc.color} mt-1`} />
                                 {doc.name}
                             </span>
                         </div>
@@ -127,12 +129,12 @@ export default function ViewPricingRequest() {
                 <h3 className="text-lg font-semibold flex text-black items-center gap-2 mb-3">
                     <Wrench className="w-5 h-5 text-blue-600" /> CFS Tools
                 </h3>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-                <button className="bg-blue-100 text-blue-600 flex items-center justify-center py-4 rounded-md gap-2"><Edit3 className="w-4 h-4" /> Edit Request</button>
-                <button className="bg-purple-100 text-purple-600 flex items-center justify-center py-4 rounded-md gap-2"><StickyNote className="w-4 h-4" /> Add Note</button>
-                <button className="bg-green-100 text-green-600 flex items-center justify-center py-4 rounded-md gap-2"><BadgeCheck className="w-4 h-4" /> Change Status: Accepted</button>
-                <button className="bg-yellow-100 text-yellow-600 flex items-center justify-center py-4 rounded-md gap-2"><MessageSquare className="w-4 h-4" /> Send Message to Customer</button>
-            </div>
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+                    <button className="bg-blue-100 text-blue-600 flex items-center justify-center py-4 rounded-md gap-2 hover:text-white hover:bg-blue-600"><Edit3 className="w-4 h-4" /> Edit Request</button>
+                    <button className="bg-purple-100 text-purple-600 flex items-center justify-center py-4 rounded-md gap-2 hover:text-white hover:bg-purple-600"><StickyNote className="w-4 h-4" /> Add Note</button>
+                    <button className="bg-green-100 text-green-600 flex items-center justify-center py-4 rounded-md gap-2 hover:text-white hover:bg-green-600"><ArrowLeftRight className="w-4 h-4" /> Change Status: Accepted</button>
+                    <button className="bg-yellow-100 text-yellow-600 flex items-center justify-center py-4 rounded-md gap-2 hover:text-white hover:bg-yellow-600"><Send className="w-4 h-4" /> Send Message to Customer</button>
+                </div>
             </div>
         </div>
     );
